4a11b1027c273a43dab5ad664a672a65f330acf7
browsable <wlsehfdl1>
2016-10-05 15:05:57 +0900

    scraping modified

--- scraping.py
+++ scraping.py
@@ -6,85 +6,69 @@ def resDownload(url): #img, js, css
     logger.info('*********** Resource Download Start ***********')
 
     r = requests.get("http://"+url,allow_redirects=True)
+
+    # scrap list : add tag if you want
+    scrapList = ['img','script','data','link']
+
+    # make directory and copy file to local
+    download(r,scrapList)
+
+    logger.info('*********** Resource Download End ***********')
+
+def download(r, scrapList):
+
     urlRedirect = r.url
 
-    originPath = 'origin/'+ urlRedirect;  # 사이트 원본 Path
-    #optPath = 'opt/' + url_redirect # 사이트 최적화 Path\
+    originPath = 'origin/' + urlRedirect;  # 사이트 원본 Path
+    # optPath = 'opt/' + url_redirect # 사이트 최적화 Path\
 
     html = r.content
     soup = BeautifulSoup(html, 'lxml')
-
-    # image
-    imgs = soup.findAll('img', {"src": True})
-    for imgURL in imgs:
-        getSrcPath(urlRedirect, imgURL, originPath, "img")
-
-    # .js
-    scripts = soup.findAll('script', {"src": True})
-    for scriptURL in scripts:
-        getSrcPath(urlRedirect, scriptURL, originPath, "js")
-
-    # .css
-    stylesheets = soup.findAll('link', {"type": "text/css", "href": True})
-    for stylesheetURL in stylesheets:
-        getHrefPath(urlRedirect, stylesheetURL , originPath)
-    logger.info('*********** Resource Download End ***********')
-
-
-def getSrcPath(urlRedirect, resUrl, originPath, type):
-    resUrl = str(resUrl)
-    resUrl = resUrl[resUrl.find("src=\"") + 5:]
-    resUrl = resUrl[:resUrl.find("\"")]
-    resUrl = resUrl.replace("../", "/")
-    logger.info(resUrl)
-    resPath = resUrl[:resUrl.rfind("/")]
-    # ex) assets/js/bootstrap.min.js
-    if ("//" not in resUrl):
-        mkdir(originPath + resPath)
-        copy_file(urlRedirect + "/" + resUrl,originPath + resUrl)
-    else:
-        # https://maps.googleapis.com/maps/api/js?key=AIzaSyBs
-        if("http" in resUrl):
-            mkdir(originPath + resPath)
-            copy_file(resUrl, originPath + resUrl)
-        # //fonts.googleapis.com/css?family=PT+Serif:regular,italic,bold,bolditalic
+    for tag in scrapList:
+        # ['img','script','data','link']
+        if(tag == 'link'):
+            srcList = soup.findAll('link', {"type": "text/css", "href": True})
         else:
-            resUrl = resUrl.replace("//", "")
-            resPath = resPath.replace("//", "")
-            mkdir(originPath + resPath)
-            copy_file("http://"+resUrl,originPath + resUrl)
+            srcList = soup.findAll(tag, {"src": True})
 
+        for src in srcList:
+            src = str(src)
+            if (tag == 'link'):
+                src = src[src.find("href=\"") + 6:]
+            else:
+                src = src[src.find("src=\"") + 5:]
+            src = src[:src.find("\"")]
+            src = src.replace("../", "/")
+            logger.info(src)
 
-def getHrefPath(urlRedirect, hrefURL, originPath):
-    hrefURL = str(hrefURL)
-    hrefURL = hrefURL[hrefURL.find("href=\"") + 6:]
-    hrefURL = hrefURL[:hrefURL.find("\"")]
-    hrefURL = hrefURL.replace("../", "/")
-    logger.info(hrefURL)
-    hrefPath = hrefURL[:hrefURL.rfind("/")]
-    # ex) assets/js/bootstrap.min.js
-    if ("//" not in hrefURL):
-        mkdir(originPath + hrefPath)
-        copy_file(urlRedirect + "/" + hrefURL , originPath + hrefURL)
-    else:
-        # ex) https://maps.googleapis.com/maps/api/js?key=AIzaSyBs
-        if("http" in hrefURL):
-            mkdir(originPath+hrefPath)
-            copy_file(hrefURL, originPath + hrefURL)
-        # ex) //fonts.googleapis.com/css?family=PT+Serif:regular,italic,bold,bolditalic
-        else:
-            hrefURL = hrefURL.replace("//", "")
-            hrefPath = hrefPath.replace("//", "")
-            mkdir(originPath + hrefPath)
-            copy_file("http://"+hrefURL,originPath + hrefURL)
+            # file path except file name
+            filePath = originPath + src[:src.rfind("/")]
+            # file path with file name
+            fileName = originPath + src
+
+            # ex) assets/js/bootstrap.min.js
+            if ("//" not in src):
+                mkdir(filePath)
+                mkfile(urlRedirect + "/" + src, fileName)
+            else:
+                # ex) https://maps.googlea....
+                if ("http" in src):
+                    mkdir(filePath)
+                    mkfile(src, fileName)
+                # ex) //fonts.googleapis....
+                else:
+                    srcPath = src[:src.rfind("/")].replace("//", "")
+                    src = src.replace("//", "")
+                    mkdir(originPath + srcPath)
+                    mkfile("http://" + src, originPath + src)
 
 def mkdir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-def copy_file(downurl,filename):
-    resource = requests.get(downurl).content
-    with open(filename, "wb") as code:
+def mkfile(downUrl,fileName):
+    resource = requests.get(downUrl).content
+    with open(fileName, "wb") as code:
         code.write(resource)
 
 def htmlMaker(html, originPath):
